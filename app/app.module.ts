775ef61f3c96f1d240ec464f5895c6eed74e6b50
Devcartha <chaouechi.souhail@gmail.com>
2016-08-25 23:41:45 +0100

    added routing + refactored templates and styles  to separate files

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,19 +2,24 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
 
+import { routing }        from './app.routing';
 
 import { AppComponent }  from './app.component';
 import { HeroService } from './hero.service';
-import { HeroDetailComponent } from './hero-detail.component';
+import { DashboardComponent } from './dashboard.component'
 import { HeroesComponent } from './heroes.component';
+import { HeroDetailComponent } from './hero-detail.component';
+
 
 @NgModule({
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    routing
   ],
   declarations: [
     AppComponent,
+    DashboardComponent,
     HeroesComponent,
     HeroDetailComponent
   ],
